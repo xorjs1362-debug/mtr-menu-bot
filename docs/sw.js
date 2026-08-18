2ce91956b96c4589813be7bb8aeef9dd7d592819
xorjs1362-debug <xorjs1362@gmail.com>
2026-08-18 17:12:19 +0900

    Bump PWA cache to v32

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='todo-pwa-v31';
+const CACHE='todo-pwa-v32';
 const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./completion-tracking.js','./manual-order.js','./mobile-layout-fix.js','./project-tab-order.js','./title-linebreak-fix.js','./weekly-report.js','./weekly-mobile-fix.js','./weekly-link-button.js','./archive-tasks.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
