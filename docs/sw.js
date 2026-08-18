469836dcea7aa50cfe4f385e605b2eaa9b34aeaa
xorjs1362-debug <xorjs1362@gmail.com>
2026-08-18 16:18:53 +0900

    Cache weekly work summary assets v27

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='todo-pwa-v26';
-const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./completion-tracking.js','./manual-order.js','./mobile-layout-fix.js','./project-tab-order.js','./title-linebreak-fix.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+const CACHE='todo-pwa-v27';
+const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./completion-tracking.js','./manual-order.js','./mobile-layout-fix.js','./project-tab-order.js','./title-linebreak-fix.js','./weekly-report.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
