c2574cd5e66c9b1ae73948e27cca9508f032ed9c
xorjs1362-debug <xorjs1362@gmail.com>
2026-08-18 15:49:57 +0900

    Refresh PWA cache to v22

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='todo-pwa-v21';
-const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./completion-tracking.js','./manual-order.js','./mobile-layout-fix.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+const CACHE='todo-pwa-v22';
+const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./completion-tracking.js','./manual-order.js','./mobile-layout-fix.js','./project-tab-order.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
