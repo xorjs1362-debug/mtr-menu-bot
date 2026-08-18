5b35519852703c762475c9806e7a3c19bf81644e
xorjs1362-debug <xorjs1362@gmail.com>
2026-08-18 11:45:59 +0900

    Finalize PWA service worker

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,8 @@
-const CACHE='todo-pwa-v1';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='todo-pwa-v2';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
@@ -14,12 +14,20 @@ self.addEventListener('activate',event=>{
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   const url=new URL(event.request.url);
-  if(url.origin!==location.origin) return;
+  if(url.origin!==self.location.origin) return;
+
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));
+    return;
+  }
+
   event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      if(response.ok){
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      }
       return response;
-    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+    }))
   );
 });
