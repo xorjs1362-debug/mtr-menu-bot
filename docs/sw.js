20c4206077395b4af569664fcf4bd4e4c8c6e8c0
xorjs1362-debug <xorjs1362@gmail.com>
2026-08-18 15:53:55 +0900

    Refresh PWA cache to v23

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='todo-pwa-v22';
+const CACHE='todo-pwa-v23';
 const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./completion-tracking.js','./manual-order.js','./mobile-layout-fix.js','./project-tab-order.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
