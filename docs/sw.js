6d689990ba5171aed659983a038e360fa5d1abb5
xorjs1362-debug <xorjs1362@gmail.com>
2026-08-18 13:41:40 +0900

    Refresh PWA cache for task UX updates

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='todo-pwa-v6';
-const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+const CACHE='todo-pwa-v7';
+const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
