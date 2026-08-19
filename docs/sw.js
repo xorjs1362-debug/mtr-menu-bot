e9f64a928d44137df60179b3dff2fa8d544c79a8
xorjs1362-debug <xorjs1362@gmail.com>
2026-08-20 08:43:57 +0900

    Cache edit modal lock in v34

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='todo-pwa-v33';
-const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./completion-tracking.js','./manual-order.js','./mobile-layout-fix.js','./project-tab-order.js','./title-linebreak-fix.js','./weekly-report.js','./weekly-mobile-fix.js','./weekly-link-button.js','./archive-tasks.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+const CACHE='todo-pwa-v34';
+const ASSETS=['./','./index.html','./style.css','./sticky-columns.css','./app.js','./app-overrides.js','./completion-tracking.js','./manual-order.js','./mobile-layout-fix.js','./project-tab-order.js','./title-linebreak-fix.js','./weekly-report.js','./weekly-mobile-fix.js','./weekly-link-button.js','./archive-tasks.js','./edit-modal-lock.js','./pwa-install.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
